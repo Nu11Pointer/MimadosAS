b2bd1379eba5826923ba00936eca183db777d4fd
Steven Mendez <stevenampaiz@gmail.com>
2022-05-30 12:39:28 -0600

    DashBoard was indexed and Login Viww now is the root page.

---
+++ AdminPresentationLayer/Scripts/Home/Home.js
@@ -0,0 +1,10 @@
+﻿// Evento Document Loaded
+document.addEventListener('DOMContentLoaded', function () {
+    SetUp();
+});
+
+// Función Principal
+function SetUp() {
+    // Pintar Menu Collapse
+    $('#nav-Dashboard').addClass('active');
+}
